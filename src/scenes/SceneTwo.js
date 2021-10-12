c28c20e8b7f754c87f4963c18638344f33943120
Savion92 <savion90@gmail.com>
2021-10-12 15:32:39 -0400

    commit for merge

--- src/scenes/SceneTwo.js
+++ src/scenes/SceneTwo.js
@@ -14,22 +14,32 @@ const SceneTwo = () => {
     <div id='sceneTwo'>
 
       <div id='coffin-div'>
-        <img src={closedCoffin} alt='an open coffin' className='coffin'/>
+        <img src={closedCoffin} alt='an open coffin' className='coffin' onClick={() => console.log('🧤 coffin')
+        } on/>
       </div>
       <div id='chair-div'>
-        <img src={oldChair} alt='a very old chair' className='oldChair'/>
+        <img src={oldChair} alt='a very old chair' className='oldChair' onClick={() => console.log('🧤 chair')
+        }/>
       </div>
       <div id='stool-div'>
-        <img src={stool} alt='a very old chair' className='stoolCabinet'/>
+        <img src={stool} alt='a very old chair' className='stoolCabinet' onClick={() => console.log('🧤 stool')
+        }/>
       </div>
       <div id='hole-div'>
-        <img src={hole} alt='an odd hole in the wall' className='holeInWall'/>
+        <img src={hole} alt='an odd hole in the wall' className='holeInWall' onClick={() => console.log('🧤 hole')
+        }/>
       </div>
       <div id='right-candle-div'>
-        <img src={wallCandle} alt='a lit candle' className='rightCandle' />
+        <img src={wallCandle} alt='a lit candle' className='rightCandle' onClick={() => console.log('🧤 right-candle')
+        }/>
       </div>
       <div id='left-candle-div'>
-        <img src={wallCandle} alt='a lit candle' className='leftCandle' />
+        <img src={wallCandle} alt='a lit candle' className='leftCandle' onClick={() => console.log('🧤 left-candle')
+        } />
+      </div>
+      <div id='painting-div'>
+        <img src={cowPainting} alt='an interesting painting' className='cowPainting' onClick={() => console.log('🧤 painting')
+        } />
       </div>
 
     </div>
