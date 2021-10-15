fede621c881e385dccd9784206b90d2ffa8ae9ad
Jason Yang <jasonyang987@gmail.com>
2021-10-15 18:05:34 -0500

    for hernan

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
 
   return (
     <div className="App">
-     {/* <Timer /> */}
+     <Timer />
       {/* <Chat /> */}
       <Router>
         <Switch>
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,26 +1,47 @@
 import "./Timer.scss";
-import { Link, BrowserRouter as Router} from "react-router-dom";
+import {useHistory} from "react-router-dom";
+import {useEffect, useState} from 'react'
 
-const Timer = ({history}) => {
-  let finished = false;
 
-  const startTimer = (duration, display) => {
-    let timer = duration,
-      minutes,
-      seconds;
-    setInterval(function () {
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-      display.textContent = minutes + ":" + seconds;
-      console.log(timer)
-      if (timer > 0) timer--;
-      else {
-        console.log("timer finished")
-      }
-    }, 1000);
-  };
+const Timer = () => {
+
+  const [seconds, setSeconds] = useState(5)
+  const history = useHistory()
+  const redirect = () => {
+    history.push('/youdied')
+  }
+  // useEffect(()=>{
+  //   let myInterval = setInterval(() => {
+  //     if (seconds > 0){
+  //       setSeconds(seconds - 1)
+  //     } else {
+  //       history.push('/youdied')
+  //     }
+  //   }, 1000)
+  //   return () => {
+  //     clearInterval(myInterval)
+  //   }
+  // })
+
+  // let finished = false;
+  // const history = useHistory()
+  // const startTimer = (duration, display) => {
+  //   let timer = duration,
+  //     minutes,
+  //     seconds;
+  //   setInterval(function () {
+  //     minutes = parseInt(timer / 60, 10);
+  //     seconds = parseInt(timer % 60, 10);
+  //     minutes = minutes < 10 ? "0" + minutes : minutes;
+  //     seconds = seconds < 10 ? "0" + seconds : seconds;
+  //     display.textContent = minutes + ":" + seconds;
+  //     console.log(timer)
+  //     if (timer > 0) timer--;
+  //     else {
+  //       history.push('/youdied')
+  //     }
+  //   }, 1000);
+  // };
 
   // window.onload = () => {
   //   const fiveMinutes = 5;
@@ -31,15 +52,10 @@ const Timer = ({history}) => {
   //   clearInterval(startTimer)
   // };
 
-    return (
-      <Router>
-      <div className="timerContainer">
-        <p id="timer"> 05:00</p>
-        <Link id = "invisible" to = "/youdied">
-        </Link>
+    return <div>
+      <p id="timer"> {seconds}</p>
+      <button onClick ={redirect}>redirect</button>
       </div>
-      </Router>
-    );
 };
 
 export default Timer;
