3e72609978e260516933e4a10cabb8f61625c875
Savion92 <savion90@gmail.com>
2021-10-18 12:06:00 -0400

    App cleaned for code review

--- src/App.js
+++ src/App.js
@@ -16,39 +16,6 @@ import Lobby from "./scenes/Lobby";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  const commands = [
-    {
-      command: ["Click on button"],
-      callback: () => clickButton(),
-    },
-  ];
-
-  let buttoney = document.getElementById("bookButton");
-
-  function clickButton() {
-    isClicked = false;
-    console.log("i'm buttoney");
-    buttoney.click();
-  }
-
-  const { transcript } = useSpeechRecognition({ commands });
-  let [isClicked] = useState(false);
-
-  const buttons = ["bookButton"];
-
-  if (isClicked) {
-    clickButton();
-  }
-
-  let speech = new SpeechSynthesisUtterance();
-  speech.lang = "en";
-  speech.text = "Im a button I am.";
-
-  function playSound(url) {
-    var a = new Audio(url);
-    a.play();
-  }
-
   return (
     <div className="App">
      {/* <Timer /> */}
@@ -68,25 +35,6 @@ function App() {
       <div className="inventory-chat">
         {/* <Inventory /> */}
       </div>
-
-      <button
-        id="bookButton"
-        className="Button"
-        onClick={() => {
-          playSound(
-            "http://www.simphonics.com/library/WaveFiles/Production%20Wavefiles/FS-98/JSPLASH2.WAV"
-          );
-        }}
-      >
-        Im a book!
-      </button>
-
-      <button onClick={SpeechRecognition.startListening}>Listen</button>
-
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={() => window.speechSynthesis.speak(speech)}>
-        My new friend the Button
-      </button>
     </div>
   );
 }
