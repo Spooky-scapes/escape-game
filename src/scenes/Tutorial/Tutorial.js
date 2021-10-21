a49dcd8739dabfb7f2cf482bf0ce7724fd25cd28
Swissius <swissius@gmail.com>
2021-10-21 12:38:37 -0400

    feat(gameplay): Added replayability and disapearing inventory

--- src/scenes/Tutorial/Tutorial.js
+++ src/scenes/Tutorial/Tutorial.js
@@ -1,71 +1,112 @@
 import React from "react";
-import {Howl, Howler} from 'howler';
+import { Howl, Howler } from "howler";
 import { Link } from "react-router-dom";
 import "./tutorial.scss";
 import SpeechRecognition, {
-    useSpeechRecognition,
-  } from "react-speech-recognition";
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const Tutorial = () => {
-    const tutorialSound = "https://firebasestorage.googleapis.com/v0/b/spooky-scapes.appspot.com/o/Spooky%20Sounds%2FSpookyTutorial.m4a?alt=media&token=26542141-47e3-460b-a2c5-57726893829b/allow-cors";
-    const tutorial = new Howl({
-        src: [tutorialSound],
-        html5: true
-    })
+  const tutorialSound =
+    "https://firebasestorage.googleapis.com/v0/b/spooky-scapes.appspot.com/o/Spooky%20Sounds%2FSpookyTutorial.m4a?alt=media&token=26542141-47e3-460b-a2c5-57726893829b/allow-cors";
+  const tutorial = new Howl({
+    src: [tutorialSound],
+    html5: true,
+  });
 
-    let playingAudio
-    const audioControl = (specifiedSound) => {
-        playingAudio = specifiedSound
-        !specifiedSound.playing() ? specifiedSound.play() : specifiedSound.stop() 
+  let playingAudio;
+  const audioControl = (specifiedSound) => {
+    playingAudio = specifiedSound;
+    !specifiedSound.playing() ? specifiedSound.play() : specifiedSound.stop();
+  };
+  const stopAllAudio = () => {
+    playingAudio.stop();
+  };
+
+  const commands = [
+    {
+      command: ["play"],
+      callback: () => clickButton(),
+    },
+  ];
+
+  useSpeechRecognition({ commands });
+
+  function clickButton() {
+    document.getElementById("playGame").click();
+  }
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+      event.preventDefault();
+      if (event.repeat) {
+        return;
+      }
+      SpeechRecognition.startListening();
     }
-    const stopAllAudio = () => {
-        playingAudio.stop()
-    }
+  });
 
-
-    const commands = [
-        {
-            command: ["play"],
-            callback: () => clickButton()
-        }
-    ];
-
-    useSpeechRecognition({commands});
-
-    function clickButton(){
-        document.getElementById("playGame").click()
-    }
-
-    document.addEventListener("keydown", (event) => {
-        if(event.code === "Space") {
-            event.preventDefault();
-            if(event.repeat){
-                return;
-            }
-            SpeechRecognition.startListening()
-        }
-    })
-
-    document.addEventListener("keyup", (event) => {
+  document.addEventListener("keyup", (event) => {
     if (event.code === "Space") {
       event.preventDefault();
       SpeechRecognition.stopListening();
-    }})
+    }
+  });
 
-    return (
-        <div>
-        <button type="button" id="playTutorial" onClick={ () => {audioControl(tutorial)}}>Play Tutorial</button>
-        <button type="button" id="pauseTutorial" onClick={ () => {stopAllAudio()}}>Stop Tutorial</button>
-        <p> To hear a description of the room, press the enter or return key. </p>
-        <p> To inspect an item, hold down the spacebar key and say ‘click on’ then the item name. </p>
-        <p> To navigate around the rooms, hold down the spacebar and say  ‘go to next room’ or go to previous room. </p>
-        <p> You can also say “go to left” or “go to right. </p>
-        <p> If you need to return to this tutorial, hold down the spacebar and say “go to tutorial". </p> 
-        <p> If you are ready to play, hold down the spacebar and say “play”. </p>
-        <Link to = "/" ><button type="button" id="goHome" > Home </button></Link>
-        <Link to = "/parlor"> <button type="button" id="playGame" > Start Playing </button></Link>
-        </div>
-    )
-}
+  const showInv = () => {
+    document.getElementsByClassName("hiddenInventory")[0].className =
+      "visInventory";
+    document.getElementsByClassName("hiddenItemBox")[0].className =
+      "visItemBox";
+  };
+
+  return (
+    <div>
+      <button
+        type="button"
+        id="playTutorial"
+        onClick={() => {
+          audioControl(tutorial);
+        }}
+      >
+        Play Tutorial
+      </button>
+      <button
+        type="button"
+        id="pauseTutorial"
+        onClick={() => {
+          stopAllAudio();
+        }}
+      >
+        Stop Tutorial
+      </button>
+      <p> To hear a description of the room, press the enter or return key. </p>
+      <p>
+        To inspect an item, hold down the spacebar key and say ‘click on’ then
+        the item name.
+      </p>
+      <p>
+        To navigate around the rooms, hold down the spacebar and say ‘go to next
+        room’ or go to previous room.
+      </p>
+      <p> You can also say “go to left” or “go to right. </p>
+      <p>
+        If you need to return to this tutorial, hold down the spacebar and say
+        “go to tutorial".
+      </p>
+      <p> If you are ready to play, hold down the spacebar and say “play”. </p>
+      <Link to="/">
+        <button type="button" id="goHome">
+          Home
+        </button>
+      </Link>
+      <Link to="/parlor">
+        <button type="button" id="playGame" onClick={() => showInv()}>
+          Start Playing
+        </button>
+      </Link>
+    </div>
+  );
+};
 
 export default Tutorial;
--- src/scenes/Victory/victory.js
+++ src/scenes/Victory/victory.js
@@ -1,15 +1,36 @@
-import './victory.scss'
-import {useHistory} from "react-router-dom";
+import "./victory.scss";
+import { useHistory } from "react-router-dom";
 
 const Victory = () => {
   const history = useHistory();
+  const resetLocal = () => {
+    window.localStorage.setItem("hasCasset", false);
+    window.localStorage.setItem("usedCasset", false);
+    window.localStorage.setItem("hasCandyBucket", false);
+    window.localStorage.setItem("usedCandyBucket", false);
+    window.localStorage.setItem("hasKey", false);
+    window.localStorage.setItem("usedKey", false);
+    window.localStorage.setItem("foundPainting", false);
+    document.getElementsByClassName("invCasset")[0].className = "hiddenCasset";
+    document.getElementsByClassName("invCandyBucket")[0].className =
+      "hiddenCandyBucket";
+    document.getElementsByClassName("invKey")[0].className = "hiddenKey";
+  };
+
   return (
     <div className="victory">
       <h1 className="vic-title">Time to Trick or Treat!</h1>
-      <h2 className = 'playAgainWin' onClick = {() => {history.push('/')}}>Play Again</h2>
+      <h2
+        className="playAgainWin"
+        onClick={() => {
+          history.push("/");
+          resetLocal();
+        }}
+      >
+        Play Again
+      </h2>
     </div>
+  );
+};
 
-  )
-}
-
-export default Victory
+export default Victory;
